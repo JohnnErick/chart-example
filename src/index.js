c39b7f93211b7817a78b7fbc23bd0e5fea84e50e
JohnnErick <jon91052@gmail.com>
2020-05-13 07:32:33 -0300

    feat(app component): separate app component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import moment from 'moment';
 
-import GlobalStyle from 'utils/globalStyles';
-import store from 'settings/configureStore';
-import Chart from 'containers/Chart';
+import App from './App';
 
 moment.locale('pt-br');
-
-function App() {
-  return (
-		<Provider store={store}>
-			<>
-				<GlobalStyle />
-				<Chart />
-			</>
-		</Provider>
-	);
-};
 
 ReactDOM.render(<App />, document.getElementById('root'));
